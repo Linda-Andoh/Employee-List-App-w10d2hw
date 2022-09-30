076e7a0ba7cee3a974757c3fc0279bef9865dbd0
Linda <lexicon736@gmail.com>
2022-09-29 20:52:34 -0400

    Wk10d2

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,16 @@
+import EmployeeListItem from "./EmployeeListItems"
+
+const EmployeeList = (props) => {
+  return (
+    <>
+        <EmployeeListItem />
+        <EmployeeListItem />
+        <EmployeeListItem />
+        <EmployeeListItem />
+        <EmployeeListItem />
+    
+    </>
+  )
+}
+
+export default EmployeeList
---
+++ src/components/EmployeeListItems.js
@@ -0,0 +1,14 @@
+const EmployeeListItems = (props) => {
+    return (
+      <>
+        <div className="employeeName">
+          <h1>Employee Name</h1>
+        </div>
+          <div className="employeeTitle">
+            <h2>Title</h2>
+          </div>
+      </>
+    )
+  }
+  
+  export default EmployeeListItems
